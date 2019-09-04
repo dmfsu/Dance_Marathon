8e6bcc4dfbe6e90ccac344395f623a8c0bfccbda
sd16g <50725184+sd16g@users.noreply.github.com>
2019-09-04 18:11:54 -0400

    update linkscreen

--- Dance/screens/LinksScreen.js
+++ Dance/screens/LinksScreen.js
@@ -1,53 +1,59 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Text, Left, Button, Body, Icon} from 'native-base';
+import { ScrollView, Image, StyleSheet } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Text, Left, Right, Button, Body, Icon} from 'native-base';
 import { ExpoLinksView } from '@expo/samples';
+
+//name, place, time, date, "points"
+//HOME PAGE
 
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
       <Container>
         <Content>
-          <Card>
-            <CardItem header>
-              <Text>Title of Event</Text>
+
+          <Card style={{ backgroundColor: '#782F40' }}>
+            <CardItem header style={{ backgroundColor: '#782F40' }}>
+              <Left><Text style={{ fontSize: 22 }}> First GBM</Text></Left>
+              <Right>
+                <Image style={{ flex: 1, height: 20, width: 60 }}
+                source={ require('../assets/images/gold_star.jpg')}/>
+                <Text> 50    </Text>
+              </Right>
             </CardItem>
-            <CardItem>
+            <CardItem style={{ backgroundColor: '#782F40'}}>
               <Body>
-                <Text>
-                  Boom Shaka Laka
-                </Text>
+                  <Image style={styles.pic} source={ require('../assets/images/cardPic.png')}/>
               </Body>
             </CardItem>
-            <CardItem footer>
-              <Left>
-                <Button transparent>
-                  <Icon active name="thumbs-up" />
-                    <Text>12 Likes</Text>
-                </Button>
-              </Left>
+            <CardItem footer style={{ backgroundColor: '#ffd700'}}>
+              <Left><Text>Oglesby Union</Text></Left>
+              <Right><Text>9/3 8:00PM - 10:00PM</Text></Right>
             </CardItem>
-         </Card>
-         <Card>
-            <CardItem header>
-              <Text>Title of Event</Text>
+
+          </Card>
+
+          <Card style={{ backgroundColor: '#782F40' }}>
+            <CardItem header style={{ backgroundColor: '#782F40' }}>
+              <Left><Text style={{ fontSize: 18 }}> Bikes 4Tykes</Text></Left>
+              <Right>
+                <Image style={{ flex: 1, height: 20, width: 60 }}
+                source={ require('../assets/images/gold_star.jpg')}/>
+                <Text> 200   </Text>
+              </Right>
             </CardItem>
-            <CardItem>
+            <CardItem style={{ backgroundColor: '#782F40' }}>
               <Body>
-                <Text>
-                  Boom Shaka Laka
-                </Text>
+                  <Image style={styles.pic} source={ require('../assets/images/bikes.jpg')}/>
               </Body>
             </CardItem>
-            <CardItem footer>
-              <Left>
-                <Button transparent>
-                  <Icon active name="thumbs-up" />
-                    <Text>12 Likes</Text>
-                </Button>
-              </Left>
+            <CardItem footer style={{ backgroundColor: '#ffd700'}}>
+              <Left><Text>Landis Green</Text></Left>
+              <Right><Text>9/10 4:00PM - 7:00PM</Text></Right>
             </CardItem>
-         </Card>
+
+          </Card>
+
         </Content>
       </Container>
     </ScrollView>
