fa8038b593b2c9a5ecf6516b3f86eec5ce66094f
sd16g <50725184+sd16g@users.noreply.github.com>
2019-09-08 14:53:35 -0400

    drop down menu w/ description

--- Dance/screens/LinksScreen.js
+++ Dance/screens/LinksScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { ScrollView, Image, StyleSheet } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Text, Left, Right, Button, Body, Icon} from 'native-base';
+import { Container, Header, Content, Card, CardItem, Text, Left, Right, Button, Body, Icon, Accordion} from 'native-base';
 import { ExpoLinksView } from '@expo/samples';
 
-//name, place, time, date, "points"
-//HOME PAGE
+const description1 = [
+  { title: "More:", content: "First General Body Meeting to go over basic details of Dance Marathon this semester, upcoming events, and more." }
+];
+
+const description2 = [
+  { title: "More:", content: "Bikes4Tykes is a charity event that helps provide bikes for underpriveleged kids. They collect donations to purchase bicycles and helmets for donation to Toys For Tots and The Salvation Army at Christmas time." }  
+];
 
 export default function LinksScreen() {
   return (
@@ -14,7 +19,7 @@ export default function LinksScreen() {
 
           <Card style={{ backgroundColor: '#782F40' }}>
             <CardItem header style={{ backgroundColor: '#782F40' }}>
-              <Left><Text style={{ fontSize: 22 }}> First GBM</Text></Left>
+              <Left><Text style={{ fontSize: 20, fontFamily: 'SavoyeLetPlain' }}> First GBM</Text></Left>
               <Right>
                 <Image style={{ flex: 1, height: 20, width: 60 }}
                 source={ require('../assets/images/gold_star.jpg')}/>
@@ -30,11 +35,14 @@ export default function LinksScreen() {
               <Left><Text>Oglesby Union</Text></Left>
               <Right><Text>9/3 8:00PM - 10:00PM</Text></Right>
             </CardItem>
+            <CardItem footer style={{ backgroundColor: '#CEB888'}}>
+              <Accordion dataArray={description1}/>
+            </CardItem>
 
           </Card>
 
           <Card style={{ backgroundColor: '#782F40' }}>
-            <CardItem header style={{ backgroundColor: '#782F40' }}>
+            <CardItem header style={{ backgroundColor: '#782F40', fontFamily: 'ChalkboardSE-Light' }}>
               <Left><Text style={{ fontSize: 18 }}> Bikes 4Tykes</Text></Left>
               <Right>
                 <Image style={{ flex: 1, height: 20, width: 60 }}
@@ -51,6 +59,9 @@ export default function LinksScreen() {
               <Left><Text>Landis Green</Text></Left>
               <Right><Text>9/10 4:00PM - 7:00PM</Text></Right>
             </CardItem>
+            <CardItem footer style={{ backgroundColor: '#CEB888'}}>
+              <Accordion dataArray={description2}/>
+            </CardItem>
 
           </Card>
 
@@ -61,7 +72,7 @@ export default function LinksScreen() {
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Events',
+  title: 'Links',
 };
 
 const styles = StyleSheet.create({
